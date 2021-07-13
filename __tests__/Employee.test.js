9c95fc875c39312c82e70e4ae0b18126aa52618e
Maggie Ward <mlward639@gmail.com>
2021-07-13 00:06:50 -0400

    engineer class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,3 @@
 const Employee = require('../lib/Employee');
 
-const nancy = new Employee('Nancy', 007, 'nancy@gmail.com')
 
-
-
-
-Nancy.getName();
-Employee.getId(Nancy);
-Employee.getEmail(Nancy);
-Employee.getRole(Nancy);
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,2 +1,4 @@
 const Employee = require('../lib/Employee');
 const Engineer = require('../lib/Engineer');
+
+Engineer.test()
